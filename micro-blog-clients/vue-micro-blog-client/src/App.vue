1dc0a0a4c50f1fad0ef930e71cca59018d27ada3
Jesus <jesws1@gmail.com>
2019-11-11 08:28:04 -0400

    add config vuejs

--- micro-blog-clients/vue-micro-blog-client/src/App.vue
+++ micro-blog-clients/vue-micro-blog-client/src/App.vue
@@ -1,13 +1,26 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
+      <router-link to="/">Home</router-link>|
       <router-link to="/about">About</router-link>
     </div>
     <router-view />
   </div>
 </template>
-
+<script>
+export default {
+  name: "app",
+  data() {
+    return {
+      id: null,
+      title: "",
+      description: "",
+      createDate: "",
+      author: ""
+    };
+  }
+};
+</script>
 <style lang="scss">
 #app {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
--- micro-blog-clients/vue-micro-blog-client/src/main.js
+++ micro-blog-clients/vue-micro-blog-client/src/main.js
@@ -2,11 +2,24 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import ApolloClient from "apollo-boost";
+import VueApollo from "vue-apollo";
 
 Vue.config.productionTip = false;
+
+const apolloClient = new ApolloClient({
+  uri: "http://localhost:4000/graphql"
+});
+
+Vue.use(VueApollo);
+
+const apolloProvider = new VueApollo({
+  defaultClient: apolloClient
+});
 
 new Vue({
   router,
   store,
-  render: h => h(App)
+  render: h => h(App),
+  apolloProvider
 }).$mount("#app");

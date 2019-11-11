202524503b8aba589dbecde41983116e95cceb19
Jesus Gilbert <jesws1@gmail.com>
2019-11-10 21:58:13 -0400

    Update port

--- micro-blog-api/index.js
+++ micro-blog-api/index.js
@@ -5,6 +5,6 @@ const  app  =  express();
 
 app.use("/graphql", ExpressGraphQL({ schema: schema.schema, graphiql: true}));
 
-app.listen(4000, () => {
+app.listen(80, () => {
     console.log("GraphQL server running at http://localhost:4000.");
 });
